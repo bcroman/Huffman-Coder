288e8d025511136a5082b1af8c698bf3a04715bd
Ben C <126107298+bcroman@users.noreply.github.com>
2025-11-16 16:46:54 +0000

    Core: Encode Message
    Created a function which will get the code index, then create the encoded message.
    output the encoded value and total bit length

--- huffmum.py
+++ huffmum.py
@@ -21,9 +21,10 @@ def read_file():
     try: # Read File
         with open("./dataset/demofile.txt", "r") as text:
             input_text = text.read()
+            return input_text
     except FileNotFoundError: #Error Handling for Missing File
         print("File not found!")
-    return input_text
+    
 
 # Function to build the frequency table
 def build_frequency_table(text):
@@ -34,21 +35,68 @@ def build_huffman_tree(frequency_table):
     priority_queue = [Node(char, freq) for char, freq in frequency_table.items()]
     heapq.heapify(priority_queue)
 
+    # Build the tree
     while len(priority_queue) > 1:
         left = heapq.heappop(priority_queue)
         right = heapq.heappop(priority_queue)
         merged = Node(None, left.freq + right.freq, left, right)
         heapq.heappush(priority_queue, merged)
 
-    return priority_queue[0]  # Root of the Huffman Tree
+    return priority_queue[0]  # Return the root value
+
+# Function to create hufffman codes
+def create_codes(root: Node) -> dict:
+    codes = {}
+
+    def traverse(node, current_code):
+        if node is None:
+            return
+
+        # Leaf node → store its code
+        if node.char is not None:
+            codes[node.char] = current_code
+            return
+
+        # Traverse left = add "0"
+        traverse(node.left, current_code + "0")
+
+        # Traverse right = add "1"
+        traverse(node.right, current_code + "1")
+
+    traverse(root, "")
+    return codes
+
+# Function to encode the input text
+def encode_message(text: str, codes: dict) -> str:
+    """
+    Encode the input text into a Huffman-encoded binary string.
+    """
+    encoded = []
+    for char in text:
+        encoded.append(codes[char])
+    return "".join(encoded)
 
 # Main Function
 if __name__ == "__main__":
     input_text = read_file()
-    print("Input Text:", input_text)
+    #print("Input Text:", input_text)
 
     frequency_table = build_frequency_table(input_text)
     print("Frequency Table:", frequency_table)
 
     huffman_tree_root = build_huffman_tree(frequency_table)
     print (" Huffman Tree Root:", huffman_tree_root.freq)
+
+    codes = create_codes(huffman_tree_root)
+
+    print("\nHuffman Codes:")
+    for char, code in codes.items():
+        printable = char if char != " " else "[space]"
+        print(f"{printable!r}: {code}")
+
+    encoded_message = encode_message(input_text, codes)
+    print("\nEncoded Message:")
+    print(encoded_message)
+    print("\nEncoded Message Length:", len(encoded_message), "bits")
+
+
